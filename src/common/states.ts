b5aa3852ee86693f91c4c27ee38703479473496e
Priya Bagus Sasikirono <priyabagus@gmail.com>
2025-04-19 15:49:42 +0700

    Add basic passenger count options

--- src/common/states.ts
+++ src/common/states.ts
@@ -1,4 +1,10 @@
 import { atom } from 'jotai'
 import FlightType from '../lib/FlightType.type'
+import CabinClass from '../lib/CabinClass.type'
 
 export const flightTypeState = atom<FlightType>('oneway')
+
+export const adultCountState = atom<number>(1)
+export const childrenCountState = atom<number>(0)
+export const infantCountState = atom<number>(0)
+export const totalPassengerCountState = atom<number>((get) => get(adultCountState) + get(childrenCountState) + get(infantCountState))
--- src/pages/Home/BoxSearchFlight.tsx
+++ src/pages/Home/BoxSearchFlight.tsx
@@ -1,13 +1,18 @@
 import { Autocomplete, Box, Button, IconButton, Input, Menu, MenuItem, Paper, Popover, Select, Stack, TextField, Typography } from '@mui/material'
 import css from './BoxSearchFlight.module.scss'
+import clsx from 'clsx'
+import { useState } from 'react'
 import FlightType from '../../lib/FlightType.type'
 import { flightTypeState } from '../../common/states'
 import { useAtom } from 'jotai'
+import { Add, ArrowDropDown, PersonOutline, Remove, TripOrigin } from '@mui/icons-material'
+import CabinClass from '../../lib/CabinClass.type'
 export default function BoxSearchFlight () {
   return (
     <Box className={css.wrapper}>
       <Stack direction='row'>
         <SelectFlightType />
+        <PassengerOptions />
       </Stack>
 
     </Box>
@@ -22,5 +27,48 @@ function SelectFlightType () {
       <MenuItem value='roundtrip'>Round trip</MenuItem>
       <MenuItem value='multicity'>Multi-city</MenuItem>
     </Select>
+  )
+}
+
+function PassengerOptions () {
+  // anchor for popover
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null)
+
+  // handle button click
+  const handleButtonClick = (evt: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(evt.currentTarget)
+  }
+
+  // handle popover close
+  const handlePopoverClose = () => setAnchorEl(null)
+
+  return (
+    <>
+      <Button variant='text' endIcon={<ArrowDropDown />} onClick={handleButtonClick}>
+        <PersonOutline />
+        <Typography>7</Typography>
+      </Button>
+
+      <Popover open={!!anchorEl} onClose={handlePopoverClose} anchorEl={anchorEl} anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }}>
+        <Paper className={css.popoverBox}>
+          <PassengerItemOption label='Adults' type='adults' />
+          <PassengerItemOption label='Children' type='childrens' />
+          <PassengerItemOption label='Infants' type='infants' />
+        </Paper>
+      </Popover>
+    </>
+  )
+}
+
+type PassengerItemOptionProps = {
+  label: string
+  type: 'adults' | 'childrens' | 'infants'
+}
+function PassengerItemOption (props:PassengerItemOptionProps) {
+  return (
+    <Stack className={css.passengerWrapper} direction='row'>
+      <Typography>{props.label}</Typography>
+      <Input className={css.passengerInput} type='number' size='small' inputProps={{ min: 0, max: 9 }} />
+    </Stack>
   )
 }
