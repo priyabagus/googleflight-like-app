65461e6af8f30784f6356d8a8d5f0016d5e8f287
Priya Bagus Sasikirono <priyabagus@gmail.com>
2025-04-21 10:14:22 +0700

    Optimization: Add debounce fetch airports to minimize API call

--- src/pages/Home/BoxSearchFlight/SelectAirport.tsx
+++ src/pages/Home/BoxSearchFlight/SelectAirport.tsx
@@ -2,11 +2,12 @@ import Airport from '@/lib/types/Airport.type'
 import getCachedSearchAirports from '@/lib/utils/cache/getCachedSearchAirports'
 import setCachedSearchLocation from '@/lib/utils/cache/setCachedSearchAirports'
 import { Autocomplete, TextField } from '@mui/material'
-import { SyntheticEvent, useState } from 'react'
+import { SyntheticEvent, useCallback, useState } from 'react'
 import capitalize from 'lodash/capitalize'
 import { destinationAirportState, errorInputState, originAirportState } from '@/common/states'
 import { useAtom } from 'jotai'
 import searchAirports from '@/lib/services/fetchRapidAPI/searchAirports'
+import debounce from 'lodash/debounce'
 
 type SelectAirportProps = {
   type: 'origin' | 'destination'
@@ -22,6 +23,24 @@ export default function SelectAirport (props:SelectAirportProps) {
   const filterOptions = (options: Airport[], state: any) => {
     return options
   }
+
+  /** function to get Option from fetching RapidAPI */
+  const getOptionsFromFetchingAPI = (searchedString:string) => {
+    (async () => {
+    // shown "Loading..." text to let user know there is a process happening
+      setTextNoOptions('Loading...')
+      // do fetch
+      const airports = await searchAirports(searchedString)
+      // make it as our shown options
+      setOptions(airports)
+      // change no options text from "Loading..." to "No options" (if there is no options to be shown)
+      setTextNoOptions('No options')
+      // save to cache
+      setCachedSearchLocation(searchedString, airports)
+    })()
+  }
+  /** Debounced search Airport for getting options */
+  const debouncedSearchAirports = useCallback(debounce(getOptionsFromFetchingAPI, 1000), [])
 
   // handleInputChange for MUI's AutoComplete component
   const handleInputChange = (evt: React.SyntheticEvent, value: string, reason: string) => {
@@ -42,20 +61,7 @@ export default function SelectAirport (props:SelectAirportProps) {
           setOptions(cachedSearch)
         } else {
           // if not exist in cache, fetch data from API
-          (async () => {
-            // shown "Loading..." text to let user know there is a process happening
-            setTextNoOptions('Loading...')
-
-            // do fetch
-            const airports = await searchAirports(value)
-
-            // make it as our shown options
-            setOptions(airports)
-            // change no options text from "Loading..." to "No options" (if there is no options to be shown)
-            setTextNoOptions('No options')
-            // save to cache
-            setCachedSearchLocation(value, airports)
-          })()
+          debouncedSearchAirports(value)
         }
       } catch (err) {
         console.log(err)
